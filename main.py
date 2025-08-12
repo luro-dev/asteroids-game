21ac6c96b2d29ab542f61e2ab3b6921521ef69b8
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2025-08-12 18:34:27 -0400

    Create basic game template
    Created the screen object with pygame then used the fill method to make the background black while also checking for user closing the window, finally refreshed the screen at the end of the while loop

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+# Allows us to use code from open source pygame library
+# throughout this file
+import pygame
+from constants import *
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+    
+    # Creating a screen object using the pygame display.set_mode method
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    print("Starting Asteroids!")
+    
+    while(True):
+        # Check if user has closed the window -> if so exits game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Makes screen black
+        screen.fill((0, 0, 0))
+
+        
+        # Refreshes the screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+
+
